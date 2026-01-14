109c6272860243cd1fcd9deeb88ae8c2fc117916
SyPhase <pi2568gamerz@gmail.com>
2026-01-13 17:35:15 -0700

    Finished part 3 of Tutorial

--- 01-07-2025-tutorial-razor-pages/Pages/Movies/Index.cshtml.cs
+++ 01-07-2025-tutorial-razor-pages/Pages/Movies/Index.cshtml.cs
@@ -8,22 +8,21 @@ using Microsoft.EntityFrameworkCore;
 using _01_07_2025_tutorial_razor_pages.Data;
 using _01_07_2025_tutorial_razor_pages.Models;
 
-namespace _01_07_2025_tutorial_razor_pages.Pages.Movies
+namespace _01_07_2025_tutorial_razor_pages.Pages.Movies;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly _01_07_2025_tutorial_razor_pages.Data._01_07_2025_tutorial_razor_pagesContext _context;
+
+    public IndexModel(_01_07_2025_tutorial_razor_pages.Data._01_07_2025_tutorial_razor_pagesContext context)
     {
-        private readonly _01_07_2025_tutorial_razor_pages.Data._01_07_2025_tutorial_razor_pagesContext _context;
+        _context = context;
+    }
 
-        public IndexModel(_01_07_2025_tutorial_razor_pages.Data._01_07_2025_tutorial_razor_pagesContext context)
-        {
-            _context = context;
-        }
+    public IList<Movie> Movie { get;set; } = default!;
 
-        public IList<Movie> Movie { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            Movie = await _context.Movie.ToListAsync();
-        }
+    public async Task OnGetAsync()
+    {
+        Movie = await _context.Movie.ToListAsync();
     }
 }
