432db9f1d22d72edd8551caafbfc5e7469b19b77
SyPhase <pi2568gamerz@gmail.com>
2026-01-15 17:52:19 -0700

    Finished part 7 of Tutorial

--- 01-07-2025-tutorial-razor-pages/Models/SeedData.cs
+++ 01-07-2025-tutorial-razor-pages/Models/SeedData.cs
@@ -5,6 +5,11 @@ using _01_07_2025_tutorial_razor_pages.Data;
 namespace RazorPagesMovie.Models;
 // Note instances of "RazorPagesMovieContext" replaced with "_01_07_2025_tutorial_razor_pagesContext"
 
+// Note when Rating was added as a new property to the Movie model, the database had to be updated via a new migration:
+//      Tools > NuGet Package Manager > Package Manager Console
+//      Add-Migration Rating
+//      Update-Database
+
 public static class SeedData
 {
     public static void Initialize(IServiceProvider serviceProvider)
@@ -30,7 +35,8 @@ public static class SeedData
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
-                    Price = 7.99M
+                    Price = 7.99M,
+                    Rating = "R"
                 },
 
                 new Movie
@@ -38,7 +44,8 @@ public static class SeedData
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
-                    Price = 8.99M
+                    Price = 8.99M,
+                    Rating = "G"
                 },
 
                 new Movie
@@ -46,7 +53,8 @@ public static class SeedData
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
-                    Price = 9.99M
+                    Price = 9.99M,
+                    Rating = "G"
                 },
 
                 new Movie
@@ -54,7 +62,8 @@ public static class SeedData
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
-                    Price = 3.99M
+                    Price = 3.99M,
+                    Rating = "NA"
                 }
             );
             context.SaveChanges();
