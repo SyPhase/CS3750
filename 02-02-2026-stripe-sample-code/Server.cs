2e6f6277e1dbe177cf969e52b8b0aea6b02d9407
SyPhase <pi2568gamerz@gmail.com>
2026-02-02 09:57:05 -0700

    Completed Stripe tutorial (sandbox example payments)

--- 02-02-2026-stripe-sample-code/Server.cs
+++ 02-02-2026-stripe-sample-code/Server.cs
@@ -7,8 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Stripe;
 using Stripe.Checkout;
-
-public class StripeOptions
+                                // See test results: https://dashboard.stripe.com/acct_1SwO0rLAJ5eoJw83/test/payments
+public class StripeOptions      // Follwing Tutorial Setup: https://docs.stripe.com/checkout/quickstart?lang=dotnet
 {
     public string option { get; set; }
 }
@@ -39,7 +39,7 @@ namespace server.Controllers
             // This test secret API key is a placeholder. Don't include personal details in requests with this key.
             // To see your test secret API key embedded in code samples, sign in to your Stripe account.
             // You can also find your test secret API key at https://dashboard.stripe.com/test/apikeys.
-            StripeConfiguration.ApiKey = "sk_test_BQokikJOvBiI2HlWgH4olfQ2";
+            StripeConfiguration.ApiKey = "sk_test_51SwO0rLAJ5eoJw83N98VDacJKxoaqmUXy6FYpslpSidnNkMRp3iYNs7b9XGOWFKpSEprsisbtOAfMlPoOJU7lp0z00YyAfssgz"; // Sandbox test key, not live
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
             app.UseRouting();
             app.UseStaticFiles();
@@ -62,17 +62,17 @@ namespace server.Controllers
                   new SessionLineItemOptions
                   {
                     // Provide the exact Price ID (for example, price_1234) of the product you want to sell
-                    Price = "{{PRICE_ID}}",
+                    Price = "price_1SwQImLAJ5eoJw83iChGYrgi", // Define a product to sell: https://docs.stripe.com/checkout/quickstart?lang=dotnet#line-items
                     Quantity = 1,
                   },
                 },
-                Mode = "payment",
-                SuccessUrl = domain + "/success.html",
+                Mode = "payment", // payment, subscription, or setup: https://docs.stripe.com/checkout/quickstart?lang=dotnet#mode
+                SuccessUrl = domain + "/success.html", // customer redirect URL on successful payment
             };
-            var service = new SessionService();
+            var service = new SessionService(); // Create checkout session
             Session session = service.Create(options);
 
-            Response.Headers.Add("Location", session.Url);
+            Response.Headers.Add("Location", session.Url); // Redirect to checkout
             return new StatusCodeResult(303);
         }
     }
