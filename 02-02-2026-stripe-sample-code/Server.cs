608d27153220461f1217f766c78131fe0e11bbb3
SyPhase <pi2568gamerz@gmail.com>
2026-02-02 09:05:27 -0700

    Started Stripe payment processing

---
+++ 02-02-2026-stripe-sample-code/Server.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Stripe;
+using Stripe.Checkout;
+
+public class StripeOptions
+{
+    public string option { get; set; }
+}
+
+namespace server.Controllers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+              .UseUrls("http://0.0.0.0:4242")
+              .UseWebRoot("public")
+              .UseStartup<Startup>()
+              .Build()
+              .Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddNewtonsoftJson();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // This test secret API key is a placeholder. Don't include personal details in requests with this key.
+            // To see your test secret API key embedded in code samples, sign in to your Stripe account.
+            // You can also find your test secret API key at https://dashboard.stripe.com/test/apikeys.
+            StripeConfiguration.ApiKey = "sk_test_BQokikJOvBiI2HlWgH4olfQ2";
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+
+    [Route("create-checkout-session")]
+    [ApiController]
+    public class CheckoutApiController : Controller
+    {
+        [HttpPost]
+        public ActionResult Create()
+        {
+            var domain = "http://localhost:4242";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                  new SessionLineItemOptions
+                  {
+                    // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+                    Price = "{{PRICE_ID}}",
+                    Quantity = 1,
+                  },
+                },
+                Mode = "payment",
+                SuccessUrl = domain + "/success.html",
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+    }
+}
